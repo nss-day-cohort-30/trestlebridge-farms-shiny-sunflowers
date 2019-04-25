ab9ed6b5c56b517a3fdb821adfda02fec38dbe9e
apple <kenperkerwicz>
2019-04-25 15:10:29 -0500

    fixed curly brace

--- src/Actions/ChoosePlowedField.cs
+++ src/Actions/ChoosePlowedField.cs
@@ -25,6 +25,9 @@ namespace Trestlebridge.Actions
       int number = Int32.Parse(Console.ReadLine());
       List<ISeedProducing> manyPlants = Enumerable.Repeat(plant, number).ToList();
 
+
+
+
       for (int i = 0; i < farm.PlowedFields.Count; i++)
       {
         if (farm.PlowedFields[i].plantCount() == 1)
@@ -39,23 +42,23 @@ namespace Trestlebridge.Actions
         {
           Console.WriteLine($"{i + 1}. Plowed Field is full. ({farm.PlowedFields[i].plantCount()} plants)");
         }
+      }
+      Console.WriteLine();
 
-        Console.WriteLine();
+      // How can I output the type of plant chosen here?
+      Console.WriteLine($"Place the {plant.Type} where?");
 
-        // How can I output the type of plant chosen here?
-        Console.WriteLine($"Place the {plant.Type} where?");
+      Console.Write("> ");
+      int choice = Int32.Parse(Console.ReadLine()) - 1;
 
-        Console.Write("> ");
-        int choice = Int32.Parse(Console.ReadLine()) - 1;
-
-        if (farm.PlowedFields[choice].plantCount() + manyPlants.Count() <= farm.PlowedFields[choice].Capacity)
-        {
-          farm.PlowedFields[choice].AddResource(manyPlants);
-        }
-        else
-        {
-          Console.Clear();
-          Console.WriteLine($@"
+      if (farm.PlowedFields[choice].plantCount() + manyPlants.Count() <= farm.PlowedFields[choice].Capacity)
+      {
+        farm.PlowedFields[choice].AddResource(manyPlants);
+      }
+      else
+      {
+        Console.Clear();
+        Console.WriteLine($@"
 ~ I'm sorry! That facility can only hold ({farm.PlowedFields[0].Capacity}) plants ~
 
 ************************************************************************
@@ -65,33 +68,30 @@ namespace Trestlebridge.Actions
 
 -----------------------((press enter to continue))----------------------
 ");
-          Console.ReadLine();
-          Console.Clear();
-          // after user hits enter ask if they want to create a new field
-          Console.WriteLine($@"
+        Console.ReadLine();
+        Console.Clear();
+        // after user hits enter ask if they want to create a new field
+        Console.WriteLine($@"
  _______________________________________________
 | Would you like to create a new Plowed Field? |
 |         Press 1 for yes or 2 for no           |
  -----------------------------------------------
 ");
-          Console.Write("> ");
-          // collect the user's input and store it in the string "input"
-          string input = Console.ReadLine();
-          // parse the string and create a switch case
-          switch (Int32.Parse(input))
-          {
-            // create a new plowedField and add it to the farm.
-            // go to the ChoosePlowedField menu and pass the animal and farm
-            case 1:
-              farm.AddPlowedField(new PlowedField());
-              ChoosePlowedField.CollectInput(farm, plant);
-              break;
-            case 2:
-              break;
-          }
+        Console.Write("> ");
+        // collect the user's input and store it in the string "input"
+        string input = Console.ReadLine();
+        // parse the string and create a switch case
+        switch (Int32.Parse(input))
+        {
+          // create a new plowedField and add it to the farm.
+          // go to the ChoosePlowedField menu and pass the animal and farm
+          case 1:
+            farm.AddPlowedField(new PlowedField());
+            ChoosePlowedField.CollectInput(farm, plant);
+            break;
+          case 2:
+            break;
         }
-
-
       }
     }
   }
