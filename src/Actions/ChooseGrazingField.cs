7a4ae284015a75c655dbc13be0b7a91669e38517
James Farrell <jamesfarrellmusic@gmail.com>
2019-04-25 10:01:42 -0500

    you can add a new grazing field if field is at capacity

--- src/Actions/ChooseGrazingField.cs
+++ src/Actions/ChooseGrazingField.cs
@@ -37,18 +37,22 @@ namespace Trestlebridge.Actions
             Console.Write("> ");
             int choice = Int32.Parse(Console.ReadLine()) - 1;
 
+            // check if grazingFields is at capacity and if so add an animal
             if (farm.GrazingFields[choice].animalCount() < farm.GrazingFields[choice].Capacity)
             {
                 farm.GrazingFields[choice].AddResource(animal);
             }
             else
+            // if GrazingFields is at capacity then don't add animal and display this message
             {
                 Console.WriteLine($@"
 *************** I'm sorry, that facility is at capacity. ***************
 **************      Please choose another facility.     ****************
 ******* If there are no other Grazing Fields available, build one.  ****
+-----------------------((press enter to continue))----------------------
               ");
                 Console.ReadLine();
+            // after user hits enter ask if they want to create a new field
                 Console.WriteLine($@"
  _______________________________________________
 | Would you like to create a new Grazing Field? |
@@ -56,10 +60,13 @@ namespace Trestlebridge.Actions
  -----------------------------------------------
 ");
                  Console.Write ("> ");
-
+            // collect the user's input and store it in the string "input"
             string input = Console.ReadLine ();
+            // parse the string and create a switch case
             switch (Int32.Parse(input))
             {
+                // create a new GrazingField and add it to the farm.
+                // go to the ChooseGrazingField menu and pass the animal and farm
                 case 1:
                     farm.AddGrazingField(new GrazingField());
                     ChooseGrazingField.CollectInput(farm, animal);
